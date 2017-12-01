e952c845dc2429af7c6a5b9ae5b672827a6731ba
Julien HOUZET <julien@teledor.fr>
2017-12-01 01:21:25 +0100

    Started response starting

---
+++ Engine/Commands/Response.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Engine.Commands
+{
+    public class Response
+    {
+        private CommandType _type;
+        private string[] Args { get; set; }
+
+        public Response(string type, params string[] args)
+        {
+            Enum.TryParse(type, out _type);
+        }
+
+        public Response()
+        {
+            
+        }
+
+        public bool Builder(string cmd)
+        {
+            var final = cmd.Split(' ');
+            Args = final.Where((src, idx) => idx != 0).ToArray();
+            return Enum.TryParse(final[0], out _type);
+        }
+    }
+}
